27f34ca367351646bf6871666e84cf1da0358e92
Arif Iqbal <arif_iqbal@yahoo.com>
2024-03-30 14:47:13 -0600

    Added all necessary UserModel Method to extract from MongoDb

--- Projects/SuggesionApp/SuggestionApp/SuggestionAppLibrary/DataAccess/MongoUserData.cs
+++ Projects/SuggesionApp/SuggestionApp/SuggestionAppLibrary/DataAccess/MongoUserData.cs
@@ -21,18 +21,33 @@ public class MongoUserData
       return results.ToList();
    } 
    
-   //Get a singal user  Asynchronously
+   //Get a singal user Asynchronously by passing a user Id and comparing it with DB.
    public async Task<UserModel> GetUser(string id)
    {
       var results = await _users.FindAsync(u => u.Id == id);
       return results.FirstOrDefault();
       
    } 
-   //Getting one User with Azure Id authentication
+   //Getting one User with Azure Id authentication  (Oject Id given by Azure AD compare it)
    public async Task<UserModel> GetUserFromAuthentication(string objectId)
    {
       var results = await _users.FindAsync(u => u.ObjectIdentifier == objectId);
       return results.FirstOrDefault();
+   }
+
+   //Create User after checking the Entry in DataBase
+
+   public Task CreateUser(UserModel user)
+   {
+      return _users.InsertOneAsync(user);
+   }
+
+   //Filter User after checking the Entry in DataBase
+
+   public Task UpdateUser(UserModel user)
+   {
+      var filter = Builders<UserModel>.Filter.Eq("Id", user.Id);
+      return _users.ReplaceOneAsync(filter, user, new ReplaceOptions { IsUpsert = true});
    }
 
 
