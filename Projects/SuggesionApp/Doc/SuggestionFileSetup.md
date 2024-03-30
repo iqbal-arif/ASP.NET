94ea4a3d6e52547c938da18a659740d5f78016e0
Arif Iqbal <arif_iqbal@yahoo.com>
2024-03-29 22:44:23 -0600

    Added DbConnection Class

--- Projects/SuggesionApp/Doc/SuggestionFileSetup.md
+++ Projects/SuggesionApp/Doc/SuggestionFileSetup.md
@@ -1,4 +1,5 @@
-BLAZOR - SuggestionAppUI
+**BLAZOR - SuggestionAppUI**
+***
 1. GlobalUsing : Setting GlobalUsings.cs to reference SuggestionAppLibrary for SuggestionAppUI and works independent of the Library but access the SuggetionAppLibray/DataAccess
 2. Dependency Injection : Set ConfigureServices() (or any other name as ConfigureInjection()) and move dependency injection from Program.cs to RegisterService.cs.  This is done to keep code clean and set separate class to reduce program.cs services.
 3. Memory Caching : in RegisterServices add AddMemoryCache()
@@ -10,7 +11,8 @@ BLAZOR - SuggestionAppUI
 
 
 
-AppLibrary - SuggestionAppLibrary
+**AppLibrary - SuggestionAppLibrary**
+***
 
 1. MODELS / SuggestionModel.cs
 2. DATA / DbConnection.cs  : Connection for MongoDb
@@ -28,3 +30,19 @@ AppLibrary - SuggestionAppLibrary
 	d. UserModel:	   properties and MongoDb Unique Identifier setup, Azuer ID (For user Identity)
 					   (SubObject) BasicSuggestionModel for Id & Suggestion Title (Extracted from SuggestionModel)
 					   (SubObect) BasicUserModel :properties and MongoDb Unique Identifier setup 
+6. DataAccess
+	1. Creates a Singleton out of the independence Injection while keeping the conneciton open continously
+	2. Change Singleton to Scoped Class where Independence Injection for every User gets a Singleton version 
+	3. Lesson 9 : Database connection class DbConneciton.cs
+
+
+
+**DataBase Connection Settings**
+***
+1. appsettings.json
+	a. "DatabaseName": "devsuggestionapp" : The name can be different for different enviornment like production database should be named differently.
+2. secrets.json (RightClick on SuggesitonAppUI/Manage User Secrets) : Contains server connection string store seperately. Don't commit to git or any online userControl. In production it is placed in CI/CD process.
+
+
+
+	
--- Projects/SuggesionApp/SuggestionApp/SuggestionAppLibrary/DataAccess/DbConnection.cs
+++ Projects/SuggesionApp/SuggestionApp/SuggestionAppLibrary/DataAccess/DbConnection.cs
@@ -1,6 +1,67 @@
 ﻿
 
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
 namespace SuggestionAppLibrary.DataAccess;
+
 public class DbConnection
 {
+   //CONSTRUCTOR FOR CONFIGURATION
+
+   
+      private readonly IConfiguration _config;
+
+      //Database connection to MongoDb
+      private readonly IMongoDatabase _db;
+
+      //Reference to appsettings.json 
+      private string _connectionId = "MongoDB";
+
+   public string DbName { get; private set; }
+
+
+   //Table Names in Database with hardcode reference : Holds Objects not row of Data
+   //The reference can be changed over here if need be. private protects the accidental name change.
+   public string CategoryCollectionName { get; private set; } = "categories";
+
+   public string StatusCollectionName { get; private set; } = "statuses";
+
+   public string UserCollectionName { get; private set; } = "users";
+   
+   public string SuggestionCollectionName { get; private set; } = "suggestions";
+
+
+   //Client connects to Database
+   public MongoClient Client { get; private set; }
+
+   public IMongoCollection<CategoryModel> CategoryColleciton { get; private set; }
+
+   public IMongoCollection<StatusModel> StatusColleciton { get; private set; }
+
+   public IMongoCollection<SuggestionModel> SuggestionColleciton { get; private set; }
+
+   public IMongoCollection<UserModel> UserCollection { get; private set; }
+
+
+   //CONSTRUCTOR FOR new Client with connection to Database and creates connections to all four collections
+   public DbConnection(IConfiguration config)
+      {
+         _config = config;
+
+         Client = new MongoClient(_config.GetConnectionString(_connectionId));
+
+         DbName = _config[key: "DatabaseName"]; //reference to appsettings.json
+
+         _db = Client.GetDatabase(DbName);
+
+         CategoryColleciton = _db.GetCollection<CategoryModel>(CategoryCollectionName);
+         StatusColleciton = _db.GetCollection<StatusModel>(StatusCollectionName);
+         UserCollection = _db.GetCollection<UserModel>(UserCollectionName);
+         SuggestionColleciton = _db.GetCollection<SuggestionModel>(SuggestionCollectionName);
+
+      }
+
+  
+
 }
