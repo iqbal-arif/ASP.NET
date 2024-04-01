ce1799ddced15d4dff5d1aca8fba89546a698168
Arif Iqbal <arif_iqbal@yahoo.com>
2024-04-01 00:24:16 -0600

    Modifying MongoSuggestionData

--- Projects/SuggesionApp/SuggestionApp/SuggestionAppLibrary/DataAccess/MongoSuggestionData.cs
+++ Projects/SuggesionApp/SuggestionApp/SuggestionAppLibrary/DataAccess/MongoSuggestionData.cs
@@ -10,7 +10,7 @@ public class MongoSuggestionData
    private readonly IMemoryCache _cache;
    private readonly IMongoCollection<SuggestionModel> _suggestions;
    private const string CacheName = "SuggestionData";
-   
+
 
    public MongoSuggestionData(IDbConnection db, IUserData userData, IMemoryCache cache)
    {
@@ -34,5 +34,61 @@ public class MongoSuggestionData
 
       return output;
 
+   }
+
+   public async Task<List<SuggestionModel>> GetAllApprovedSuggestions()
+   {
+
+      var output = await GetAllSuggestions();
+      return output.Where(x => x.ApprovedForRelease).ToList();
+
+   }
+   
+   //One Suggestion
+   public async Task<SuggestionModel> GetSuggestion(string id)
+   {
+      var results = await _suggestions.FindAsync(s => s.Id == id);
+      return results.FirstOrDefault();
+   }
+
+   public async Task<List<SuggestionModel>> GetAllSuggestionsWaitingForApproval()
+   {
+      var output = await GetAllSuggestions();
+
+      return output.Where(x => x.ApprovedForRelease == false && x.Rejected == false).ToList();
+   }
+
+
+   public async Task UpdateSuggestion(SuggestionAppLibrary suggestion)
+   {
+      await _suggestions.ReplaceOneAsync(s => s.Id == suggestion.Id, suggestion);
+      _cache.Remove(CacheName);
+
+   }
+
+   public async Task UpvoteSuggestion( string suggestionId , string userId)
+   {
+      var client = _db.Client;
+
+      using var session = await client.StartSessionAsync();
+
+      session.StartTransaction();
+
+      try
+      {
+         var db = client.GetDatabase(_db.DbName);
+
+         var suggestionInTransaction = db.GetCollection<SuggestionModel>(_db.SuggestionCollectionName);
+
+         var suggestion = (await suggestionInTransaction.FindAsync(s => s.Id == suggestionId)).First();
+
+         bool isUpvote = suggestion.UserVotes.Add(userId);
+         if 
+      }
+      catch (Exception ex)
+      {  
+         await session.AbortTransactionAsync();
+         throw;
+      }
    }
 }
