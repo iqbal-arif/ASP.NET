2107b3a79d8162c0554bca70d4dc7f3c6f39e993
Arif Iqbal <arif_iqbal@yahoo.com>
2024-04-13 14:07:07 -0600

    Create.razor file and Modified Details.razor file

---
+++ Projects/SuggesionApp/SuggestionApp/SuggestionAppUI/Models/CreateSuggestionModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SuggestionAppUI.Models;
+
+//LESSON 16
+
+//CreatSuggesionModel UI Specific need; otherwise we could have used SuggestionModel from AppLibrary
+public class CreateSuggestionModel
+{
+   [Required]
+   [MaxLength(75)]
+   public string Suggestion { get; set; }
+
+   [Required]
+   [MinLength(1)]
+   [Display(Name ="Category")]
+   public string CategoryId { get; set; }
+
+   [MaxLength(500)]
+   public string Description { get; set; }
+}
